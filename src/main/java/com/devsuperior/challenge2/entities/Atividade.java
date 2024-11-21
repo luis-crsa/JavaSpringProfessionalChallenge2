808ad0e133a347463a940c647af11bb3f0154137
Luis <luiscrsarmento@gmail.com>
2024-11-21 12:26:42 -0300

    Participante, Muitos para muitos, id update

--- src/main/java/com/devsuperior/challenge2/entities/Atividade.java
+++ src/main/java/com/devsuperior/challenge2/entities/Atividade.java
@@ -1,7 +1,9 @@
 package com.devsuperior.challenge2.entities;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,6 +11,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -34,6 +38,12 @@ public class Atividade {
 
     @OneToMany(mappedBy = "atividade")
     private List<Bloco> blocos = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_atividade_participante",
+            joinColumns = @JoinColumn(name = "atividade_id"),
+            inverseJoinColumns = @JoinColumn(name = "participante_id"))
+    private Set<Participante> participantes = new HashSet<>();
 
     public Atividade() {
     }
@@ -89,5 +99,9 @@ public class Atividade {
     public List<Bloco> getBlocos() {
         return blocos;
     }
-    
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
+    }
+
 }
--- src/main/java/com/devsuperior/challenge2/entities/Participante.java
+++ src/main/java/com/devsuperior/challenge2/entities/Participante.java
@@ -1,34 +1,39 @@
 package com.devsuperior.challenge2.entities;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_categoria")
-public class Categoria {
-
+@Table(name = "tb_participante")
+public class Participante {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String descricao;
+    private String nome;
 
-    @OneToMany(mappedBy = "categoria")
-    private List<Atividade> atividades = new ArrayList<>();     
-    
-    public Categoria() {
+    @Column(unique = true)
+    private String email;
+
+    @ManyToMany(mappedBy = "participantes")
+    private Set<Atividade> atividades = new HashSet<>();
+
+    public Participante() {
     }
 
-    public Categoria(Integer id, String descricao) {
+    public Participante(Integer id, String nome, String email) {
         this.id = id;
-        this.descricao = descricao;
+        this.nome = nome;
+        this.email = email;
     }
 
     public Integer getId() {
@@ -39,15 +44,23 @@ public class Categoria {
         this.id = id;
     }
 
-    public String getDescricao() {
-        return descricao;
+    public String getNome() {
+        return nome;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public List<Atividade> getAtividades() {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Atividade> getAtividades() {
         return atividades;
     }
 
@@ -67,7 +80,7 @@ public class Categoria {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Categoria other = (Categoria) obj;
+        Participante other = (Participante) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
@@ -75,5 +88,5 @@ public class Categoria {
             return false;
         return true;
     }
-
+    
 }
